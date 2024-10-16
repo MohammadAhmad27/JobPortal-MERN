294b2d869e20c0c983e1f3eba522f3ca9ff92267
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-10-16 22:55:03 +0500

    Added vite, tailwind and shadcn/ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-700 text-5xl text-center'>Lets built Job Portal</h1>
+    </>
+  )
+}
+
+export default App

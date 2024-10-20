9206d018a5ce6ff8360d9b345a69904605e00818
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-10-20 15:27:11 +0500

    Added Multer middleware for file uploads, updated user route to use Multer and added sonner component

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
+import { singleUpload } from "../middleware/multer.js";
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", singleUpload, register);
 router.post("/login", login);
 router.get("/logout", logout)
 router.put("/profile/update", isAuthenticated, updateProfile);

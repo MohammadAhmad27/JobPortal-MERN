4908c7580786ce6ea16c8d50762e7b7e249dc0d4
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-10-20 23:46:54 +0500

    Updated Jobs page with new components and added custom scrollbar

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -1,7 +1,35 @@
+import FilterCard from '@/components/jobs/FilterCard'
+import JobCard from '@/components/jobs/JobCard'
+import Navbar from '@/components/shared/Navbar'
 import React from 'react'
 
 export default function Jobs() {
+    const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8]
     return (
-        <div>Jobs</div>
+        <>
+            <Navbar />
+            <div className='max-w-7xl mx-10 mt-5'>
+                <div className="flex gap-5">
+                    <div className="w-[20%]">
+                        <FilterCard />
+                    </div>
+                    {
+                        !jobsArray.length ? (
+                            <span className="text-center text-gray-500">No jobs found</span>
+                        ) : (
+                            <div className='flex-1 h-[88vh] overflow-y-auto pb-5 custom-scroll'>
+                                <div className="grid grid-cols-3 gap-4">
+                                    {jobsArray.map((item, index) => (
+                                        <div key={index}>
+                                            <JobCard />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        </>
     )
 }
